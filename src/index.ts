8c86f66e1e7086e0e320c00ba290e89e557ee82e
Jan Krupiński <jk.krupinski@gmail.com>
2021-12-02 00:22:45 +0100

    Auth: Connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express from 'express';
 import authRouter from './app/auth/_routes';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import config from './config';
+
+mongoose
+  .connect(config.CONNECTION_STRING)
+  .then(() => console.log('connection succesful'))
+  .catch(err => console.error(err));
 
 const app = express();
 
