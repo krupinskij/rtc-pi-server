34e168fb220c646cda98ef95ddd09702c199146e
Jan Krupiński <jk.krupinski@gmail.com>
2021-12-01 21:51:01 +0100

    Auth: Set login template

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import authRouter from './auth/auth.routes';
 
 const app = express();
+
+app.use('/api/auth', authRouter);
 
 app.listen(3000, () => {
   console.log(`App started on port 3000`);
