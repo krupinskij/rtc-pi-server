cb9ca7a8d4bbe6692a9f982551b29c95ffbfbfad
Jan Krupiński <50505013+krupinskij@users.noreply.github.com>
2021-12-05 21:12:24 +0100

    Revert "Auth"

--- src/exception/http.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpStatus } from './model';
-
-export default class HttpException extends Error {
-  constructor(message: string, public httpStatus: HttpStatus) {
-    super(message);
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,7 @@
 import express from 'express';
-import authRouter from './app/auth/_routes';
-import { json } from 'body-parser';
-import mongoose from 'mongoose';
-import config from './config';
-import cors from 'cors';
-
-mongoose
-  .connect(config.CONNECTION_STRING)
-  .then(() => console.log('connection succesful'))
-  .catch(err => console.error(err));
 
 const app = express();
 
-app.use(
-  cors({
-    origin: '*',
-  })
-);
-
-app.use(json());
-
-app.use('/api/auth', authRouter);
-
-app.listen(3030, () => {
-  console.log(`App started on port 3030`);
+app.listen(3000, () => {
+  console.log(`App started on port 3000`);
 });
