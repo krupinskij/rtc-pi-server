6f2a86b53bf4d30fc52db38c0d68e0be59ed37ef
Jan Krupiński <jk.krupinski@gmail.com>
2021-12-28 19:52:19 +0100

    I18n: Translate error messages

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import cors from 'cors';
 import express from 'express';
 import { authenticate } from 'middleware/authenticate';
 import mongoose from 'mongoose';
+import i18next from 'i18next';
+import i18nextMiddleware from 'i18next-http-middleware';
+import Backend from 'i18next-fs-backend';
 
 import authRouter from './app/auth/auth.routes';
 import cameraRouter from './app/camera/camera.routes';
@@ -19,9 +22,21 @@ mongoose
   .then(() => console.log('Connection established succesfully'))
   .catch((err) => console.error(err));
 
+i18next
+  .use(Backend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init({
+    backend: {
+      loadPath: __dirname + '/locales/{{lng}}.json',
+    },
+    fallbackLng: 'pl',
+    load: 'languageOnly',
+  });
+
 const app = express();
 app.use(json());
 app.use(cookieParser());
+app.use(i18nextMiddleware.handle(i18next));
 
 app.use(
   cors({
