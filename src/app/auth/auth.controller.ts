ea4c6eacf317d0259e8b19562250570c71baecc3
Jan Krupiński <jk.krupinski@gmail.com>
2022-01-13 23:06:33 +0100

    Deploy: Set same site

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -24,7 +24,7 @@ const login = async (req: Request<LoginInput>, res: Response<HeaderTokens>) => {
         maxAge: 60 * 60 * 1000,
         httpOnly: true,
         secure: true,
-        sameSite: false,
+        sameSite: 'none',
       })
       .send({ accessToken, csrfToken });
   } catch (error: any) {
@@ -50,11 +50,13 @@ const register = async (req: Request<RegisterInput>, res: Response<HeaderTokens>
         maxAge: 5 * 60 * 1000,
         httpOnly: true,
         secure: true,
+        sameSite: 'none',
       })
       .cookie('refresh-token', refreshToken, {
         maxAge: 60 * 60 * 1000,
         httpOnly: true,
         secure: true,
+        sameSite: 'none',
       })
       .send({ accessToken, csrfToken });
   } catch (error: any) {
@@ -79,11 +81,13 @@ const refresh = async (req: AuthRequest, res: Response<HeaderTokens | string>) =
         maxAge: 5 * 60 * 1000,
         httpOnly: true,
         secure: true,
+        sameSite: 'none',
       })
       .cookie('refresh-token', refreshToken, {
         maxAge: 60 * 60 * 1000,
         httpOnly: true,
         secure: true,
+        sameSite: 'none',
       })
       .send({ accessToken, csrfToken });
   } catch (error: any) {
